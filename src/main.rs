1589aa70908fd2039fcd258951117ff578623372
Matteo Di Noia <dmatteo002@gmail.com>
2024-11-03 09:50:54 +0100

    Format file by cargo fmt -all

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 #![allow(dead_code)]
 mod types;
-use crate::types::message::MessageContent;
-use crate::types::message::MessageRequest;
-use crate::types::message::DataResponse;
 
 fn main() {
     println!("Hello, world!");
--- src/types/message.rs
+++ src/types/message.rs
@@ -3,63 +3,66 @@ use crate::types::SourceRoutingHeader;
 
 // Server is multype
 #[derive(Debug)]
-pub struct ServerType{ // 1 or more must be true
-	is_chat_server: bool,
-	is_text_server: bool, // must be true if media is true
-	is_media_server: bool
-}
-
-
-#[derive(Debug)]
-pub struct Message{
-	message_data: MessageData,
-	routing_header: SourceRoutingHeader
+pub struct ServerType {
+    // 1 or more must be true
+    is_chat_server: bool,
+    is_text_server: bool, // must be true if media is true
+    is_media_server: bool,
 }
 
 #[derive(Debug)]
-pub struct MessageData { // Only part fragmentized
-	source_id: NodeId,
-	session_id: u64,
-	content: MessageContent
+pub struct Message {
+    message_data: MessageData,
+    routing_header: SourceRoutingHeader,
 }
 
 #[derive(Debug)]
-pub enum MessageContent{
-	// Client -> Server
-	ReqServerType,
-	ReqFilesList,
-	ReqFile(u64),
-	ReqMedia(u64),
-
-	ReqClientList,
-	ReqMessageSend { to: NodeId, message: Vec<u8> },
-	// Do we need request of new messages? or directly sent by server?
-
-	// Server -> Client
-	RespServerType(ServerType),
-	RespFilesList(Vec<u64>),
-	RespFile(Vec<u8>),
-	RespMedia(Vec<u8>),
-	ErrUnsupporedRequestType,
-	ErrRequestedNotFound,
-
-	RespClientList(Vec<NodeId>),
-	RespMessageFrom { from: NodeId, message: Vec<u8> },
-	ErrWrongClientId,
+pub struct MessageData {
+    // Only part fragmentized
+    source_id: NodeId,
+    session_id: u64,
+    content: MessageContent,
 }
 
+#[derive(Debug)]
+pub enum MessageContent {
+    // Client -> Server
+    ReqServerType,
+    ReqFilesList,
+    ReqFile(u64),
+    ReqMedia(u64),
 
-impl Message{
-	pub fn new(routing_header: SourceRoutingHeader, source_id: NodeId, session_id: u64, content: MessageContent) -> Self{
-		Self{
-			routing_header,
-			message_data: MessageData{
-				source_id,
-				session_id,
-				content
-			},
-		}
-	}
+    ReqClientList,
+    ReqMessageSend { to: NodeId, message: Vec<u8> },
+    // Do we need request of new messages? or directly sent by server?
+
+    // Server -> Client
+    RespServerType(ServerType),
+    RespFilesList(Vec<u64>),
+    RespFile(Vec<u8>),
+    RespMedia(Vec<u8>),
+    ErrUnsupporedRequestType,
+    ErrRequestedNotFound,
+
+    RespClientList(Vec<NodeId>),
+    RespMessageFrom { from: NodeId, message: Vec<u8> },
+    ErrWrongClientId,
 }
 
-
+impl Message {
+    pub fn new(
+        routing_header: SourceRoutingHeader,
+        source_id: NodeId,
+        session_id: u64,
+        content: MessageContent,
+    ) -> Self {
+        Self {
+            routing_header,
+            message_data: MessageData {
+                source_id,
+                session_id,
+                content,
+            },
+        }
+    }
+}
