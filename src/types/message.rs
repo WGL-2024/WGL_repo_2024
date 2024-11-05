155d85c35f5971bde2c27ea32b6fca00905294a7
Matteo Di Noia <matteodinoia02@gmail.com>
2024-11-05 17:17:49 +0100

    Cosmetic changes

--- src/types/message.rs
+++ src/types/message.rs
@@ -65,29 +65,29 @@ impl Message {
             },
         }
     }
-  
+
+    //takes message and returns the data struct serialized in a String
+    //so it goes from the actual data struct to a String
     #[allow(unused_variables)]
     fn serialize(&self) -> String {
         unimplemented!()
     }
-    //takes message and returns the data struct serialized in a String
-    //so it goes from the actual data struct to a String
-    #[allow(unused_variables)]
 
+    //Takes the content String and makes an instance of Message from it
+    #[allow(unused_variables)]
     fn deserialize(serialized: String) -> Message {
         unimplemented!()
     }
-    //Takes the content String and makes an instance of Message from it
-    #[allow(unused_variables)]
 
+    //takes the String and splits it into Fragments
+    #[allow(unused_variables)]
     fn disassembly(serialized: String) -> Vec<Fragment> {
         unimplemented!()
     }
-    //takes the String and splits it into Fragments
-    #[allow(unused_variables)]
 
+    //takes a bunch of Fragments and composes them in a serialized string.
+    #[allow(unused_variables)]
     fn assembly(fragments: Vec<Fragment>) -> String {
         unimplemented!()
     }
-    //takes a bunch of Fragments and composes them in a serialized string.
 }
