bca43881c0c92dfbf86db20d7264e34afc564408
Suge42 <lsandrini04@gmail.com>
2024-11-08 21:21:34 +0100

    Drone API

--- src/api/drone.rs
+++ src/api/drone.rs
@@ -1,5 +1,20 @@
-use crate::types::packet::Packet;
+use crate::types::channel::Channel;
+use crate::types::NodeId;
+use std::collections::HashMap;
+use std::thread;
 
-pub trait DroneAble {
-    fn forward_packet(&self, packet: Packet) -> bool;
+pub struct Drone {
+    id: NodeId,
+    thread: thread::JoinHandle<()>,
+    channels: HashMap<u8, Channel>,
+}
+
+trait DroneTrait {
+    fn new(drone_id: u8) -> Self;
+    // The thread would be created inside here,
+    // giving him reference to the list of channels,
+    // that would be expanded during the creation of the
+    // later drones.
+    fn add_channel(new_channel: Channel) -> Result<String, String>;
+    fn remove_channel(drone_id: u8) -> Result<String, String>;
 }
