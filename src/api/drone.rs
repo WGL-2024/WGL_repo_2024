38e767cf27b515ce9cfd38f485a482d60d6f625d
Suge42 <145698910+Suge42@users.noreply.github.com>
2024-11-13 19:36:53 +0100

    Revert "Revert "Making implementation simpler and usable with more than a single drone""

--- src/api/drone.rs
+++ src/api/drone.rs
@@ -1,26 +1,21 @@
 use crate::types::command::Command;
-use crate::types::packet::Packet;
 use crate::types::source_routing_header::NodeId;
 use crossbeam_channel::{Receiver, Sender};
-use std::collections::HashMap;
+use crate::types::packet::Packet;
 
-//This struct will be used inside the threads created by the initializer.
-pub struct Drone {
-    pub drone_id: NodeId,
-    pub sim_contr_send: Sender<Command>,   //Not packet.
-    pub sim_contr_recv: Receiver<Command>, //Not packet.
-    pub packet_send: HashMap<NodeId, Sender<Packet>>, //All the sender to other nodes.
-    pub packet_recv: Receiver<Packet>, //This drone receiver, that will be linked to a sender given to every other drone.
-    pub pdr: u8,                       //Would keep it in % to occupy less space, but could be f32.
-}
-
-pub trait DroneImplement {
-    fn new_drone(
+// This is a drone of a group
+// Pass to it only what it need to know
+pub trait Drone{
+    fn new(
         id: NodeId,
-        scs: Sender<Command>,
-        scr: Receiver<Command>,
-        ps: HashMap<NodeId, Sender<Packet>>,
-        pr: Receiver<Packet>,
+        sim_contr_send: Sender<Command>,
+        sim_contr_recv: Receiver<Command>,
+        packet_recv: Receiver<Packet>,
         pdr: f32,
-    ) -> Drone;
+    ) -> Self;
+    // Other nodes are added by sending command
+    // using the simulation control channel to send
+    // Command(AddChannel(...))
+
+    fn run(&mut self);
 }
