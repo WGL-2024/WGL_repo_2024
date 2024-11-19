3dca95caf59a7b2f82239252cecece79b80554bd
Matteo Di Noia <dmatteo002@gmail.com>
2024-11-19 18:00:20 +0100

    making hops public

--- crates/wg_network/src/routing.rs
+++ crates/wg_network/src/routing.rs
@@ -3,5 +3,5 @@ pub type NodeId = u8;
 #[derive(Debug)]
 pub struct SourceRoutingHeader {
     /// Initiator and nodes to which the packet will be forwarded to.
-    hops: Vec<NodeId>,
+    pub hops: Vec<NodeId>,
 }
