2e16ba425cb4bf53c7263d39c0cfc74ee60ac1e6
daw-dev <dawdev04@gmail.com>
2024-11-22 17:28:44 +0100

    Server simplification example

--- crates/wg_packet/src/message.rs
+++ crates/wg_packet/src/message.rs
@@ -21,38 +21,44 @@ pub trait MessageContent {
         Self: Sized;
 }
 
+pub trait Request: MessageContent {}
+pub trait Response: MessageContent {}
+
 // ReqServerType,
 #[derive(Debug, Clone)]
 pub enum TextRequest {
-    FileList,
-    File(u64),
+    TextList,
+    Text(u64),
 }
 
 impl MessageContent for TextRequest {
     fn serialize(&self) -> String {
         match self {
-            TextRequest::FileList => "FileList".to_string(),
-            TextRequest::File(id) => format!("File({})", id),
+            TextRequest::TextList => "TextList".to_string(),
+            TextRequest::Text(id) => format!("Text({})", id),
         }
     }
 
     fn deserialize(serialized: String) -> Result<Self, String> {
-        if serialized == "FileList" {
-            Ok(TextRequest::FileList)
+        if serialized == "TextList" {
+            Ok(TextRequest::TextList)
         } else if let Ok(id) = serialized
-            .trim_start_matches("File(")
+            .trim_start_matches("Text(")
             .trim_end_matches(')')
             .parse()
         {
-            Ok(TextRequest::File(id))
+            Ok(TextRequest::Text(id))
         } else {
             Err("Failed to deserialize TextRequest".to_string())
         }
     }
 }
 
+impl Request for TextRequest {}
+
 #[derive(Debug, Clone)]
 pub enum MediaRequest {
+    MediaList,
     Media(u64),
 }
 
@@ -65,11 +71,13 @@ impl MessageContent for MediaRequest {
     }
 }
 
+impl Request for MediaRequest {}
+
 #[derive(Debug, Clone)]
 pub enum ChatRequest {
     ClientList,
     Register(NodeId),
-    SendMessage { to: NodeId, message: String },
+    SendMessage { from: NodeId, to: NodeId, message: String },
 }
 
 impl MessageContent for ChatRequest {
@@ -81,14 +89,16 @@ impl MessageContent for ChatRequest {
     }
 }
 
+impl Request for ChatRequest {}
+
 #[derive(Debug, Clone)]
-pub enum FileResponse {
-    FileList(Vec<u64>),
-    File(String),
+pub enum TextResponse {
+    TextList(Vec<u64>),
+    Text(String),
     NotFound,
 }
 
-impl MessageContent for FileResponse {
+impl MessageContent for TextResponse {
     fn serialize(&self) -> String {
         todo!()
     }
@@ -97,8 +107,11 @@ impl MessageContent for FileResponse {
     }
 }
 
+impl Response for TextResponse {}
+
 #[derive(Debug, Clone)]
 pub enum MediaResponse {
+    MediaList(Vec<u64>),
     Media(Vec<u8>), // should we use some other type?
 }
 
@@ -111,10 +124,13 @@ impl MessageContent for MediaResponse {
     }
 }
 
+impl Response for MediaResponse {}
+
 #[derive(Debug, Clone)]
 pub enum ChatResponse {
-    RespClientList(Vec<NodeId>),
-    RespMessageFrom { from: NodeId, message: Vec<u8> },
+    ClientList(Vec<NodeId>),
+    MessageFrom { from: NodeId, message: Vec<u8> },
+    MessageSent,
 }
 
 impl MessageContent for ChatResponse {
@@ -123,5 +139,79 @@ impl MessageContent for ChatResponse {
     }
     fn deserialize(_: String) -> Result<Self, String> {
         todo!()
+    }
+}
+
+impl Response for ChatResponse {}
+
+mod example {
+    use wg_network::{NodeId, SourceRoutingHeader};
+    use wg_network::topology::ServerType;
+    use crate::{ChatRequest, ChatResponse, Message, MessageContent, MessageData, Request, Response};
+
+    trait Server {
+        type RequestType: Request;
+        type ResponseType: Response;
+
+        fn compose_message(routing_header: SourceRoutingHeader, source_id: NodeId, session_id: u64, raw_content: String) -> Result<Message<Self::RequestType>, String> {
+            let content = Self::RequestType::deserialize(raw_content)?;
+            Ok(Message {
+                routing_header,
+                message_data: MessageData {
+                    session_id,
+                    source_id,
+                    content,
+                },
+            })
+        }
+
+        fn on_request_arrived(&mut self, routing_header: SourceRoutingHeader, source_id: NodeId, session_id: u64, raw_content: String) {
+            if raw_content == "ServerType" {
+                let _server_type = Self::get_sever_type();
+                // send response
+                return;
+            }
+            match Self::compose_message(routing_header, source_id, session_id, raw_content) {
+                Ok(message) => self.send_response(self.handle_request(message.message_data)),
+                Err(str) => panic!(str)
+            }
+        }
+
+        fn send_response(&mut self, _response: Self::ResponseType) {
+            // send response
+        }
+
+        fn handle_request(&mut self, request: Self::RequestType) -> Self::ResponseType;
+
+        fn get_sever_type() -> ServerType;
+    }
+
+    struct ChatServer;
+
+    impl Server for ChatServer {
+        type RequestType = ChatRequest;
+        type ResponseType = ChatResponse;
+
+        fn handle_request(&mut self, request: Self::RequestType) -> Self::ResponseType {
+            match request {
+                ChatRequest::ClientList => {
+                    println!("Sending ClientList");
+                    ChatResponse::ClientList(vec![1, 2])
+                }
+                ChatRequest::Register(id) => {
+                    println!("Registering {}", id);
+                    ChatResponse::ClientList(vec![1, 2])
+                }
+                ChatRequest::SendMessage { message, to, from: _ } => {
+                    println!("Sending message \"{}\" to {}", message, to);
+                    // effectively forward message
+                    ChatResponse::MessageSent
+                }
+            }
+        }
+
+        fn get_sever_type() -> ServerType {
+            ServerType::Chat
+        }
     }
 }
