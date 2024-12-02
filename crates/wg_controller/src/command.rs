15c41f26da967f0d1ec093164a327dca1cec72c1
Suge42 <145698910+Suge42@users.noreply.github.com>
2024-12-02 18:06:35 +0100

    Update command.rs

--- crates/wg_controller/src/command.rs
+++ crates/wg_controller/src/command.rs
@@ -5,7 +5,7 @@ use wg_packet::Packet;
 /// From controller to drone
 #[derive(Debug, Clone)]
 pub enum DroneCommand {
-    CloseChannel(NodeId),
+    RemoveSender(NodeId),
     AddSender(NodeId, Sender<Packet>),
     SetPacketDropRate(f32),
     Crash,
