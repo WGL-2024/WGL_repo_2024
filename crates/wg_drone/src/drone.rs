cba07e63f46b36047feddebf29d37953bbb624ca
din-oia <matteodinoia02@gmail.com>
2024-12-27 09:11:52 +0100

    Allow sending

--- crates/wg_drone/src/drone.rs
+++ crates/wg_drone/src/drone.rs
@@ -7,7 +7,7 @@ use wg_packet::Packet;
 
 /// This is the drone interface.
 /// Each drone's group must implement it
-pub trait Drone {
+pub trait Drone : Send {
     /// The list packet_send would be crated empty inside new.
     /// Other nodes are added by sending command
     /// using the simulation control channel to send 'Command(AddChannel(...))'.
